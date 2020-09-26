88f780d70400efc7a83f3d6f066eba25a7f082f6
skj1991 <stacykjohns@icloud.com>
2020-09-26 17:18:07 -0400

    fixed header in index.html file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("connected!")
+const endPoint = "http://localhost:3000/api/v1/trips";
+
+document.addEventListener('DOMContentLoaded',() => {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(trips => {
+        console.log(trips);
+    })
+})
